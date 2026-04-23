ae8465ac53247bc5354c4d9e95db9789f9060591
isham2512 <ishamsingh9026@gmail.com>
2026-04-23 11:09:56 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,12 +2,10 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-
-    console.log("✅ MongoDB connected");
-    console.log("📦 DB Name:", conn.connection.name);
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log(" MongoDB Connected");
   } catch (error) {
-    console.error("❌ MongoDB error:", error.message);
+    console.error(" MongoDB Error:", error);
     process.exit(1);
   }
 };
